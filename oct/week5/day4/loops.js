e7eec066b0500b335478db9a9fe67b34495ae8f3
tripathyroja04 <rojatripathy89@gmail.com>
2025-10-31 13:53:49 +0530

    operator in js

---
+++ oct/week5/day4/loops.js
@@ -0,0 +1,32 @@
+console.log("we are gping to learn loops");
+// for loop
+
+// let i;
+// for (let i = 1; i <= 10;) {
+//   console.log("Hello", i)
+//   i++
+// }
+// console.log(i);
+
+// while loop
+// let j = 1;
+// while (j <= 10) {
+//   console.log("Qlith");
+//   j++;
+// }
+
+// let arr=[10,20,30,40,50];
+// while(arr.length <0){
+//   console.log(arr);
+// }
+
+// for (let i = 0; i <= 100; i++) {
+//     if (i % 2 === 0) 
+//     console.log(i);
+// }
+
+let value = 100;
+while (value != 100) {
+  console.log("you have entered a wrong value");
+    value = prompt("Enter the value");
+}
